2ee3e931f0b33710a7cd5adc8b99d7b27cea3da2
zacherycecil <zcjl98@yahoo.ca>
2022-11-08 18:04:27 -0400

    Implementing dynamic routing using react-router to create multiple pages. Unfinished

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { NavLink, Routes, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
@@ -8,7 +9,47 @@ function App() {
 	fetch("/api/summoner/afrozone").then(res => res.json()).then(data => {
 		setString1(data[0])
 	})
-  }, [string1]);
+  }, []);
+
+  const Navigation = () => (
+    <nav>
+      <ul>
+        <li><NavLink to='/'>Home</NavLink></li>
+        <li><NavLink to='/about'>About</NavLink></li>
+        <li><NavLink to='/contact'>Contact</NavLink></li>
+      </ul>
+    </nav>
+  );
+
+  const Home = () => (
+      <div className='home'>
+        <h1>Welcome to my portfolio website</h1>
+        <p> Feel free to browse around and learn more about me.</p>
+      </div>
+    );
+
+    const About = () => (
+      <div className='about'>
+        <h1>About Me</h1>
+        <p>Ipsum dolor dolorem consectetur est velit fugiat. Dolorem provident corporis fuga saepe distinctio ipsam? Et quos harum excepturi dolorum molestias?</p>
+        <p>Ipsum dolor dolorem consectetur est velit fugiat. Dolorem provident corporis fuga saepe distinctio ipsam? Et quos harum excepturi dolorum molestias?</p>
+      </div>
+    );
+
+    const Contact = () => (
+      <div className='contact'>
+        <h1>Contact Me</h1>
+        <p>You can reach me via email: <strong>hello@example.com</strong></p>
+      </div>
+    );
+
+  const Main = () => (
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/about' element={<About/>}></Route>
+        <Route path='/contact' element={<Contact/>}></Route>
+      </Routes>
+    );
 
   return (
     <div className="App">
